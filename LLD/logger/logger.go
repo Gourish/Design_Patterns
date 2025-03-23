301faf4046da92d7dcdb174d1fb34eb1926caaa5
Gourish <gourishhebbar@gmail.com>
2025-03-23 13:07:31 +0530

    Add files via upload

--- LLD/logger/logger.go
+++ LLD/logger/logger.go
@@ -1,1 +1,41 @@
+package logger
 
+import (
+	"fmt"
+	"os"
+	"sync"
+)
+
+type Log struct {
+	logFile *os.File
+}
+
+var log Log
+var once sync.Once
+
+func init() {
+	log = Log{}
+}
+
+type ILog interface {
+	WriteToLog(data string) error
+}
+
+func (log *Log) WriteToLog(data string) error {
+
+	_, err := log.logFile.WriteString(data)
+	return err
+
+}
+
+func GetLogger() ILog {
+	once.Do(func() {
+		logfile, err := os.Create("log.txt")
+		if err != nil {
+			fmt.Println("error creating log file ", err)
+			return
+		}
+		log.logFile = logfile
+	})
+	return &log
+}
