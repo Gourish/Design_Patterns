46aa1dc451e88ae1efeb4d2be1b1270310f10fa5
Gourish <gourishhebbar@gmail.com>
2025-03-23 13:06:24 +0530

    Add files via upload

--- LLD/differentPackage/testlog.go
+++ LLD/differentPackage/testlog.go
@@ -1,1 +1,11 @@
+package differentpackage
 
+import "example.logger.com/logger"
+
+type TestLog struct {
+	Log logger.ILog
+}
+
+func (tl *TestLog) PrintToTxt(str string) {
+	tl.Log.WriteToLog(str)
+}
